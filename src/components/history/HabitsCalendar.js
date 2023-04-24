be2211caabb5045c1229ae50907f578dede830a2
Paula Rodrigues <pullynnhah@icloud.com>
2023-04-24 19:38:20 -0300

    feat: build calendar for history

---
+++ src/components/history/HabitsCalendar.js
@@ -0,0 +1,33 @@
+import dayjs from "dayjs";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { useNavigate } from "react-router-dom";
+import "../../styles/css/calendar.css";
+export default function HabitsCalendar({ redDates, greenDates, setDisplayDate }) {
+  const navigate = useNavigate();
+
+  function changeDate(e) {
+    const calendarDay = dayjs(e).format("DD/MM/YYYY");
+    if (dayjs().format("DD/MM/YYYY") === calendarDay) navigate("/today");
+    else {
+      setDisplayDate(calendarDay);
+    }
+  }
+
+  function getClassName({ date }) {
+    const calendarDay = dayjs(date).format("DD/MM/YYYY");
+    if (dayjs().format("DD/MM/YYYY") === calendarDay) return "";
+    if (redDates.includes(calendarDay)) return "red-circle";
+    return greenDates.includes(calendarDay) ? "green-circle" : "";
+  }
+
+  return (
+    <Calendar
+      locale="pt-BR"
+      onClickDay={changeDate}
+      tileClassName={getClassName}
+      tileContent={<div></div>}
+      formatDay={(locale, date) => dayjs(date).format("DD")}
+    />
+  );
+}
--- src/pages/HistoryPage.js
+++ src/pages/HistoryPage.js
@@ -1,13 +1,64 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import Page from "../components/page/Page";
 
 import { UserContext } from "../contexts/UserContext";
+import { PageTitle } from "../styles/PageTitle";
+import styled from "styled-components";
+import HabitsCalendar from "../components/history/HabitsCalendar";
+import { getHistory } from "../services/api";
 
 export default function HistoryPage() {
   const [isLoading, setIsLoading] = useState(false);
+  const [history, setHistory] = useState(null);
+  const [redDates, setRedDates] = useState([]);
+  const [greenDates, setGreenDates] = useState([]);
+  const [displayDate, setDisplayDate] = useState(null);
+  const {
+    user: { token }
+  } = useContext(UserContext);
 
-  const { user } = useContext(UserContext);
+  useEffect(() => {
+    setIsLoading(true);
+    getHistory(token)
+      .then(res => {
+        const newHistory = res.data.reduce(
+          (acc, data) => ({
+            ...acc,
+            [data.day]: data.habits.map(h => ({ name: h.name, done: h.done }))
+          }),
+          {}
+        );
 
-  return <Page isLoading={isLoading}>HistoryPage</Page>;
+        const red = [];
+        const green = [];
+
+        for (const date in newHistory) {
+          if (newHistory[date].some(h => !h.done)) red.push(date);
+          else green.push(date);
+        }
+
+        setHistory(newHistory);
+        setRedDates(red);
+        setGreenDates(green);
+        setIsLoading(false);
+      })
+      .catch(err => {
+        alert(err);
+        setIsLoading(false);
+      });
+  }, [token]);
+
+  return (
+    <Page isLoading={isLoading}>
+      <Header>
+        <PageTitle>Histórico</PageTitle>
+      </Header>
+      <HabitsCalendar redDates={redDates} greenDates={greenDates} setDisplayDate={setDisplayDate} />
+      {/* <HistoryHabits /> */}
+    </Page>
+  );
 }
+const Header = styled.header`
+  margin: 28px 0 11px;
+`;
